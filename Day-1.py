dac699ee697b195125bba9d5037a1f3fa916492a
AnilIsukapudi <113751964+AnilIsukapudi@users.noreply.github.com>
2023-02-02 18:53:53 +0530

    Add files via upload

---
+++ Day-1.py
@@ -0,0 +1,58 @@
+num1=int(input("enter first integer:"))
+num2=int(input("enter second integer:"))
+num3=int(input("enter third integer:"))
+x=float(input("enter first float:"))
+y=float(input("enter second float:"))
+z=float(input("enter third float:"))
+str1=input("enter first string:")
+str2=input("enter second string:")
+complex1=complex(input("enter complex number:"))
+print("first integer:",num1)
+print("second integer:",num2)
+print("third integer:",num3)
+print("first float:",x)
+print("second float:",y)
+print("third float:",z)
+print("first string:",str1)
+print("second string:",str2)
+print("complex number:",complex1)
+
+
+n1,n2,s1=int(input("enter n1:")),3,input("enter string:")
+print(n1)
+print(n2)
+print(s1)
+a,b=input("enter:").split(",")
+print(a,b)
+
+
+x=3*-31.2
+print(x)
+
+x=36.32
+y=x*3+56.19-10
+print("final value:",y)
+
+l=int(input("enter the length:"))
+b=int(input("enter the breadth:"))
+print("area of the rectangle is",l*b)
+print("perimeter is:",2*(l+b))
+
+'''a=10
+b=20
+temp=b
+b=a
+a=temp
+print(a,b)'''
+a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print(a,b)
+
+
+
+
+
+        
